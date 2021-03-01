b62b2bc05a9ca2a475814745617683cf487f92d0
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2021-02-28 18:54:14 -0600

    Added MOD10 Counter

--- P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/xsim_webtalk.tcl
+++ P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Feb 28 18:06:18 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Feb 28 18:47:31 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "9695deabd0ac5f8f99823de3d1bc8448" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "9c0b14be83164f998277aa0049a008a2" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "26" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "29" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7700 CPU @ 3.60GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "17.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1180 ns" -context "xsim\\usage"
-webtalk_add_data -client xsim -key iteration -value "4" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.16_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6060_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4111188698 -regid "" -xml C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key runtime -value "1050 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6024_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2530446483 -regid "" -xml C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/A01039835/Documents/OneDrive_ITESM/Instituto Tecnologico y de Estudios Superiores de Monterrey/Silvestre Daniel Muñoz Olivo - LAB_SISTEMAS_DIGITALES_OneDrive/P5/P5.sim/sim_1/behav/xsim/xsim.dir/Counter_MOD6_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
